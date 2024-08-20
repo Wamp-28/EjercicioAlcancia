36aa806127735ee15493782e0baf86d01e0d3587
William Alexander Matallana <wmatallana28@gmail.com>
2024-08-20 14:11:43 -0500

    Ajuste nombre

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // William Alexnader Matallana Porras
+        // Ejercicio Alcancia
+        // 20-08-2024
 
-
         System.out.println();
         System.out.println();
         System.out.println();
         System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-
-
-
 
 
     }
